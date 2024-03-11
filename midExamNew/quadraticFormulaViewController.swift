8695517e1769c13860f1557a8780a6548d790cd8
Pgsavaliya <pgsavaliya01@gmail.com>
2024-03-11 14:23:08 -0400

    second push

--- midExamNew/quadraticFormulaViewController.swift
+++ midExamNew/quadraticFormulaViewController.swift
@@ -11,7 +11,6 @@ class quadraticFormulaViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
@@ -23,43 +22,39 @@ class quadraticFormulaViewController: UIViewController {
     @IBOutlet weak var txtC: UITextField!
     
     @IBAction func btnCalculate(_ sender: Any) {
-        
-        if(txtA.text == "" && txtB.text == "" && txtC.text == "")
-        {
+        if !(txtA.text?.isEmpty ?? true) || !(txtB.text?.isEmpty ?? true) || !(txtC.text?.isEmpty ?? true), let textA = txtA.text, let textB = txtB.text, let textC = txtC.text {
             
-            if let textA = txtA.text, let textB = txtB.text, let textC = txtC.text, let a = Double(textA), let b = Double(textB), let c = Double(textC) {
-                let discriminant = b * b - 4 * a * c
-                
-                if discriminant < 0 {
-                    txtInvisible.text = "There are no real roots since the discriminant is less than zero."
-                } else if discriminant == 0 {
-                    let root = (-b + sqrt(discriminant)) / (2 * a)
-                    //let rootB = (-b - sqrt(discriminant)) / (2 * a)
-                    txtInvisible.text = "There is one real root: \(root)"
-                } else {
-                    let root1 = (-b + sqrt(discriminant)) / (2 * a)
-                    let root2 = (-b - sqrt(discriminant)) / (2 * a)
-                    txtInvisible.text = "There are two real roots: \(root1) and \(root2)"
-                }
-            } else {
-                if let textA = txtA.text, let textB = txtB.text, let textC = txtC.text {
-                    if let a = Double(textA){
-                        txtInvisible.text = "The value you entered for A is invalid."
-                        txtInvisible.textColor = UIColor.red
-                    }else if let b = Double(textB){
-                        txtInvisible.text = "The value you entered for B is invalid."
-                        txtInvisible.textColor = UIColor.red
-                    }else{
-                        txtInvisible.text = "The value you entered for C is invalid."
-                        txtInvisible.textColor = UIColor.red
+                if let a = Double(textA), let b = Double(textB), let c = Double(textC) {
+                    let discriminant = b * b - 4 * a * c
+                    
+                    if discriminant < 0 {
+                        txtInvisible.text = "There are no real roots since the discriminant is less than zero."
+                    } else if discriminant == 0 {
+                        let root = (-b + sqrt(discriminant)) / (2 * a)
+                        txtInvisible.text = "There is one real root: \(root)"
+                    } else {
+                        let root1 = (-b + sqrt(discriminant)) / (2 * a)
+                        let root2 = (-b - sqrt(discriminant)) / (2 * a)
+                        txtInvisible.text = "There are two real roots: \(root1) and \(root2)"
                     }
+                } else {
+                    txtInvisible.text = ""
+                    if Double(textA) == nil {
+                        txtInvisible.text = "The value you entered for A is invalid."
+                    }
+                    if Double(textB) == nil {
+                        txtInvisible.text += "\nThe value you entered for B is invalid."
+                    }
+                    if Double(textC) == nil {
+                        txtInvisible.text += "\nThe value you entered for C is invalid."
+                    }
+                    txtInvisible.textColor = .red
                 }
+               
+            } else {
+                txtInvisible.text = "Enter a value for A , B and to find X."
+                txtInvisible.textColor = .red
             }
-        
-        }else{
-            txtInvisible.text = "Enter valid values for A, B, and C"
-            txtInvisible.textColor = UIColor.red
-        }
     }
     
     
